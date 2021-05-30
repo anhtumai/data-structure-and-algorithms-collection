99915e363d4e2c9c5e9e3b221d97243d1aee60cb
anhtumai <tu.mai@outlook.com>
2021-05-30 12:53:57 +0300

    Add in place quicksort with partition

--- divide_and_conquer/quick_sort.py
+++ divide_and_conquer/quick_sort.py
@@ -20,6 +20,30 @@ def stable_quick_sort(arr: list[any]) -> list[any]:
             equal.append(x)
     return stable_quick_sort(smaller) + equal + stable_quick_sort(bigger)
 
+# Pseudo code for Partition
+# Partition (A, left, right) [input = A[left .. right]]
+# - pivot := A[left]
+# - i := left + 1
+# - for j = l + 1 to r
+#       - if A[j] < p
+#           - A[i], A[j] = A[j], A[i]
+#           - i := i + 1
+# - A[left], A[i-1] = A[i-1], A[left]
+
 
 def in_place_quick_sort(arr: list) -> None:
-    pass
+    _partition(arr, 0, len(arr) - 1)
+
+
+def _partition(arr: list, left: int, right: int) -> None:
+    if left >= right:
+        return
+    pivot = arr[left]
+    i = left + 1
+    for j in range(left+1, right + 1):
+        if arr[j] < pivot:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+    arr[left], arr[i-1] = arr[i-1], arr[left]
+    _partition(arr, left, i - 2)
+    _partition(arr, i, right)
