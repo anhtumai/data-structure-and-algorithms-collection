63735e7f12350cccd952262b7ee69bcca8a6d687
anhtumai <tu.mai@outlook.com>
2021-06-19 00:37:11 +0300

    Add sanity check for minmax_heap

--- heap/unittest_heap.py
+++ heap/unittest_heap.py
@@ -2,13 +2,24 @@ import unittest
 from minmax_heap import MinHeap, MaxHeap
 
 
-class TestHeap(unittest.TestCase):
+class Node(object):
+    def __init__(self, name: str, distance: int):
+        self.name = name
+        self.distance = distance
 
+    def __repr__(self):
+        return f"({self.name},{self.distance})"
+
+    def __lt__(self, other):
+        return self.distance < other.distance
+
+
+class TestHeap(unittest.TestCase):
     def test_min_heap(self):
         xs = [10, 3, 5, 2, 19, 13, 1]
         min_heap = MinHeap(xs)
         sorted_xs = sorted(xs)
-        for x in sorted_xs:
+        for x in sorted_xs: 
             self.assertEqual(x, min_heap.poll())
 
     def test_max_heap(self):
@@ -18,12 +29,13 @@ class TestHeap(unittest.TestCase):
         for x in sorted_xs[::-1]:
             self.assertEqual(x, max_heap.poll())
 
-    def test_replace(self):
-        xs = [4, 2, 5, 19, 42, 3, 1]
-        min_heap = MinHeap(xs)
-        print(str(min_heap))
-        min_heap.replace(3, 0)
-        print(str(min_heap))
+    def test_decrease_key(self):
+        nodes = [Node("R", -1), Node("B", 6), Node("A", 3), Node("X", 1), Node("E", 4)]
+        min_heap = MinHeap(nodes)
+        min_heap.decrease_key("B", -17)
+        root = min_heap.peek()
+        self.assertEqual(root.name, "B")
+        self.assertEqual(root.distance, -17)
 
 
 if __name__ == "__main__":
